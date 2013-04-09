e91421c6ceda1248a12ba0403e529850b0a1af2e
Ryder Moody <rydthemoodster@gmail.com>
2013-04-09 00:12:54 -0400

    Redis working in development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  $redis_raw = Redis.new(:host => "10.0.1.1", :port => 6379)
-  $redis = Redis::Namespace.new(:ns, :redis => $redis_raw)
-
-  def redis
-    Redis.new(:host => "10.0.1.1", :port => 6380)
-  end
 end
