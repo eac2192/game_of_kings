1b2efb197f3a6f2c61ac7e1c02615cc8d99656a4
Ryder Moody <rydthemoodster@gmail.com>
2013-04-08 23:40:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameOfKings::Application.config.secret_key_base = 'e7e8501dac50f460e9ca60b50e8398728daa27068626ed3a65987bfeb59a1e5295e9d3266a32c39718e5f5186614857c584306afd2e58777b5d51a8e6ae561af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GameOfKings::Application.config.session_store :encrypted_cookie_store, key: '_GameOfKings_session'
